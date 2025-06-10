bfeb51c161ce3b6803c2cafb074414457b6e9931
Devashish Pandey <devashishpandey33@gmail.com>
2025-06-10 12:45:00 +0530

    index is done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+connectDB();
+
+app.get("/", (req,res) => res.send("API is Running...."));
+
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server is runinng on port  ${PORT} `));
